8b8c6c7623eb617ee48b8ef622f68482bbddd5c4
Suellen Santana <suellensantana18@gmail.com>
2025-09-02 10:23:23 -0300

    hero

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'))
+const imagein =  require('gulp-imagemin');
 
 
 
@@ -8,7 +9,14 @@ function styles(){
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(gulp.dest('./dist/css'))
 }
-exports.default = styles;
+
+function images(){
+    return gulp.src('./src/images/**/')
+    .pipe(imagein())
+    .pipe(gulp.dest('./dist/css'))
+}
+
+exports.default = gulp.parallel(styles, images);
 
 exports.watch = function (){
 gulp.watch('./src/styles/*.scss,', gulp.parallel(styles))
